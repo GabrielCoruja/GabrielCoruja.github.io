56b16909e1e7787b63bde24c00406fb9cd57e13b
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-09-24 14:48:18 -0300

    correção do exercicios

--- exercises/3_1/exercicio8.js
+++ exercises/3_1/exercicio8.js
@@ -1,6 +1,6 @@
 let numero = 12, numero2 = 11, numero3 = 15;
 
-if ((numero2 % 2 || numero3 % 2 || numero % 2) === 0){
+if (numero2 % 2 === 0 || numero3 % 2 === 0 || numero % 2 === 0){
     console.log(2===2);
 }
 else {
