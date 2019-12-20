151fe9163f10a091e087642fd8dd7d2b983a992b
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-12-20 18:55:18 -0300

    Iniciando implementaçã no estilo do github

--- pixel-art-project/script.js
+++ pixel-art-project/script.js
@@ -5,10 +5,12 @@ const buttonClear = document.getElementsByClassName("button-clear")[0];
 const buttonCreatSquare = document.getElementsByClassName("button-create-square-pixel")[0];
 const table = document.getElementById('tabela2');
 const tbody = document.getElementsByTagName('tbody')[0];
+const input = document.getElementById('input');
+
 let selectColor = "black";
 pintarTodasCores();
 
-for(let index = 1; index < color.length; index++) {
+for (let index = 1; index < color.length; index++) {
   let red = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
@@ -17,10 +19,23 @@ for(let index = 1; index < color.length; index++) {
 }
 
 buttonCreatSquare.addEventListener('click', () => {
-  tbody.remove();
+  while (table.hasChildNodes()) {
+    table.removeChild(table.firstChild);
+  }
+  let tbodyNew = document.createElement('tbody');
+  table.appendChild(tbodyNew)
+  for (let index = 0; index < input.value; index++) {
+    const tags = document.createElement('tr');
+    tbodyNew.appendChild(tags);
+    for (let index = 0; index < input.value; index++) {
+      const tag = document.createElement('td');
+      tags.appendChild(tag);
+    }
+  }
+  pintarTodasCores()
 })
 
-for(let paint of color) {
+for (let paint of color) {
   paint.addEventListener('click', () => {
     let computedStyle = getComputedStyle(paint);
     selectColor = computedStyle.backgroundColor;
@@ -37,8 +52,7 @@ function pintarTodasCores() {
 }
 
 buttonClear.addEventListener('click', () => {
-  for( let index = 0; index < printando.length; index++) {
-    printando[index].style.background = 'white'
+  for (let index = 0; index < printando.length; index++) {
+    printando[index].style.background = 'white';
   }
 })
-
