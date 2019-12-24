1f58d21847b0f24009c9148efa6a390920ba9433
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-12-24 18:39:36 -0300

    Falta adicionar 1 requisito

--- projetos/project-todo-list/script.js
+++ projetos/project-todo-list/script.js
@@ -1,56 +1,70 @@
-const tag = document.getElementById("lista-de-elementos-adicionados");
+const tagOl = document.getElementById("lista-de-elementos-adicionados");
 const textoInput = document.getElementById("adicionar-tarefa");
 const gerarLista = document.getElementsByClassName("estilo-botoes")[0];
-const removerSelecionados = document.getElementsByClassName("estilo-botoes")[1];
 const removerLista = document.getElementsByClassName("estilo-botoes")[2];
 const removerItem = document.getElementsByClassName("estilo-botoes")[3];
+const removeElementsSelecteds = document.getElementById(
+  "delete-elements-selected"
+);
 let itemSelected = "";
 
 gerarLista.addEventListener("click", () => {
   let li = document.createElement("li");
   li.innerHTML = textoInput.value;
-  tag.appendChild(li);
+  tagOl.appendChild(li);
   textoInput.value = "";
   generateStyle();
-  gerar();
+  elementCompleted();
+  delectedAll();
+  deleteElementsCompleted(li);
 });
 
 const generateStyle = () => {
   const list = document.getElementsByTagName("li");
   for (let clas of list) {
     clas.addEventListener("click", () => {
-      for(let index = 0; index < list.length; index++){
-          list[index].style.background = '';
+      for (let index = 0; index < list.length; index++) {
+        list[index].style.background = "";
       }
       clas.style.background = "gray";
       itemSelected = clas;
+      deleteSelected();
     });
   }
 };
 
-const gerar = () => {
-    const list = document.getElementsByTagName("li");
-    for (let clas of list) {
+const elementCompleted = () => {
+  const list = document.getElementsByTagName("li");
+  for (let clas of list) {
     clas.addEventListener("dblclick", () => {
-        if (clas.style.textDecoration) {
-          clas.style.textDecoration = "";
-          clas.style.color = "";
-          console.log('zero')
-        } else {
-          clas.style.textDecoration = "line-through";
-          clas.style.color = "red";
-          console.log('colorido')
-        }
-      });
+      clas.style.textDecoration = "line-through";
+      clas.style.color = "red";
+    });
+  }
+};
+
+function deleteElementsCompleted(li) {
+  removeElementsSelecteds.addEventListener("click", () => {
+    const list = document.getElementsByTagName("li");
+    for (let index = 0; index < list.length; index++) {
+      if (list[index].style.color) {
+        list[index].remove();
+        index--;
+      }
     }
+  });
 }
 
-removerItem.addEventListener('click', () => {
+function deleteSelected() {
+  removerItem.addEventListener("click", () => {
     itemSelected.remove();
-})
+  });
+}
 
-removerLista.addEventListener("click", function() {
-  while (tag.firstChild) {
-    tag.firstChild.remove();
-  }
-});
+function delectedAll() {
+  removerLista.addEventListener("click", function() {
+    while (tag.firstChild) {
+      tag.firstChild.remove();
+    }
+  });
+}
