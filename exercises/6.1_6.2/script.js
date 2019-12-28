c90f8fe68e5ea36b48622ff1cef64d17c56c6912
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-12-27 23:54:38 -0300

    Finalizando 6.1

--- exercises/6.1_6.2/script.js
+++ exercises/6.1_6.2/script.js
@@ -1,53 +1,108 @@
- 
 let estadosBrasileiros = {
-    'AC': 'Acre',
-    'AL': 'Alagoas',
-    'AP': 'Amapá',
-    'AM': 'Amazonas',
-    'BA': 'Bahia',
-    'CE': 'Ceará',
-    'DF': 'Distrito Federal',
-    'ES': 'Espírito Santo',
-    'GO': 'Goiás',
-    'MA': 'Maranhão',
-    'MT': 'Mato Grosso',
-    'MS': 'Mato Grosso do Sul',
-    'MG': 'Minas Gerais',
-    'PA': 'Pará',
-    'PB': 'Paraíba',
-    'PR': 'Paraná',
-    'PE': 'Pernambuco',
-    'PI': 'Piauí',
-    'RJ': 'Rio de Janeiro',
-    'RN': 'Rio Grande do Norte',
-    'RS': 'Rio Grande do Sul',
-    'RO': 'Rondônia',
-    'RR': 'Roraima',
-    'SC': 'Santa Catarina',
-    'SP': 'São Paulo',
-    'SE': 'Sergipe',
-    'TO': 'Tocantins'
-}
+  AC: "Acre",
+  AL: "Alagoas",
+  AP: "Amapá",
+  AM: "Amazonas",
+  BA: "Bahia",
+  CE: "Ceará",
+  DF: "Distrito Federal",
+  ES: "Espírito Santo",
+  GO: "Goiás",
+  MA: "Maranhão",
+  MT: "Mato Grosso",
+  MS: "Mato Grosso do Sul",
+  MG: "Minas Gerais",
+  PA: "Pará",
+  PB: "Paraíba",
+  PR: "Paraná",
+  PE: "Pernambuco",
+  PI: "Piauí",
+  RJ: "Rio de Janeiro",
+  RN: "Rio Grande do Norte",
+  RS: "Rio Grande do Sul",
+  RO: "Rondônia",
+  RR: "Roraima",
+  SC: "Santa Catarina",
+  SP: "São Paulo",
+  SE: "Sergipe",
+  TO: "Tocantins"
+};
 
-for(let ganha in estadosBrasileiros) {
-  let estado = document.createElement('option');
+for (let ganha in estadosBrasileiros) {
+  let estado = document.createElement("option");
   estado.innerHTML = ganha;
   estado.value = estadosBrasileiros[ganha];
   document.getElementById("opçao-de-estados").appendChild(estado);
 }
 
-const date = document.getElementById("data");
-const dataValid = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
 const submit = document.getElementById("submit");
+const clear = document.getElementById("reset");
+const data = document.getElementById("dado");
+const datas = document.getElementById("datas");
+const label = document.getElementsByTagName("label");
+const input = document.getElementsByTagName("input");
+const tagSpan = document.getElementsByTagName("span");
+const clas = document.getElementsByClassName("last-job");
+const select = document.getElementById("opçao-de-estados");
+const reset = document.getElementById("reset");
+const arrayInput = [input[5], input[6]]
 
-submit.addEventListener('change', () => {
-    date = date.value.toUpperCase()
-})
+submit.addEventListener("click", event => {
+  console.log(input)
+  console.log(input[8].pattern)
+  if(input[8].pattern == input[8].value) {
+    alert("falta")
 
+  }
+  event.preventDefault();
 
-submit.addEventListener('click', () => {
-  if(!dataValid.test(date.this.value)) {
-    alert("Data informada é inválida");
-    console.log(date.value)
-  };
 });
+
+function dataEntered(text, value, number) {
+  for (let index = 0; index < text.length - number; index++) {
+    const tagP = document.createElement("p");
+    tagP.innerHTML = `${text[index].textContent} ${value[index].value}`;
+    datas.appendChild(tagP);
+  }
+}
+
+function valueEstate() {
+  const tagP = document.createElement("p");
+  tagP.innerHTML = `Estado: ${select.value}`;
+  datas.appendChild(tagP);
+}
+
+// for(let complement of arrayInput) {
+//   complement.addEventListener('click', display)
+// }
+
+// function display() {
+//   return complement.value
+// }
+// // function valor() {
+// //   for(let complement of arrayInput) {
+// //     complement.addEventListener('click', function() {
+
+      
+// //       return complement.value
+// //     })
+// //   }
+// // }
+
+data.addEventListener("click", () => {
+  while (datas.hasChildNodes()) {
+    datas.removeChild(datas.firstChild);
+  }
+  dataEntered(label, input, 2);
+  const tagP = document.createElement("p");
+  tagP.innerHTML = `Complemento:`;
+  datas.appendChild(tagP);
+  valueEstate();
+  dataEntered(tagSpan, clas, 0);
+});
+
+reset.addEventListener('click', () => {
+  while (datas.hasChildNodes()) {
+    datas.removeChild(datas.firstChild);
+  }
+})
